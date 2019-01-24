93e4bde65b91919274f9046f0534d39d4c446cd4
Nuala Gray and Randee Layosa <nuala@>
2019-01-24 11:43:30 -0800

    Add split and shift methods to break up inputted words and shift the first letter of the word to the end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var example1 = "Swirl";
+var letters = example1.split(""); //a
+var shiftLetters = letters.shift(); //b
+letters[letters.length] = shiftLetters;
+console.log(letters);
+
+
+var translation = [];
+
+
+
 var pigLatin = function(word) {
+  if (word.char[])
   return pigLatin.push("way");
 });
