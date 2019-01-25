57aa83d63dd5a3b6774f932f4ccb268b54d6813d
Nuala Gray and Randee Layosa <nuala+randee@>
2019-01-24 17:04:00 -0800

    Try again

--- js/scriptsNuala.js
+++ js/scriptsNuala.js
@@ -1,46 +1,3 @@
-//starts with 1 consonant
-function(userInput){
-
-  var userInputString = "swirl";
-  var userInputArray = userInputString.split("");  //a //turns into an array
-  var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
-
-
-  var word = "";
-  var startsWithVowel = false;
-
-  //starts with a vowel
-  vowels.forEach(function(vowel) {
-    if (vowel === userInputArray[0]) {
-      word = userInputArray.join("") + "way";
-      startsWithVowel = true;
-    }
-  });
-
-// "s" "w" "i" "r" "l"
-// 0    1   2   3   4
-// Starts with consonant
-
-  vowels.forEach(function(vowel) {
-    if (!startsWithVowel === userInputArray[0]) {
-    var shiftLettersB = userInputArray.shift(); //b
-    userInputArray[userInputArray.length] = shiftLettersB;
-    word = userInputArray.join("") + "ay";
-    console.log(word);
-  });
-
-  console.log(startsWithVowel);
-  console.log(shiftLettersB);
-  console.log(word);
-}
-
-}
-
-
-
-
-
-
 //Nuala's
 
 //starts with 1 consonant
@@ -48,7 +5,6 @@ function(userInput){
 
   var userInputString = "swirl";
   var userInputArray = userInputString.split("");  //a //turns into an array
-  var translation = [];
   var vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
   var wentThroughOnce = false;
 
